a94fe49bfd5d3247686d511b2dde3893204f1dd4
NaldoHoracio <edvonaldohs@ic.ufal.br>
2018-09-04 15:52:58 -0300

    Priority queue

---
+++ QUEUE/priority_queue.c
@@ -0,0 +1,22 @@
+/*
+  * Create by NaldoHorácio
+  * 04 September 2018
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_PQ_SIZE 100
+
+struct Item
+{
+  int item;
+  int priority_item;
+};
+
+typedef struct Item item_pq;
+
+struct priorit_queue
+{
+
+};
