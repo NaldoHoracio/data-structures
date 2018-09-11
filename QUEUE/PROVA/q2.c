8a09e40db1e96ccf3e1558312cdac9ce10d16dc2
NaldoHoracio <edvonaldohs@ic.ufal.br>
2018-09-11 15:42:03 -0300

    Atualização da questão 2.

--- QUEUE/PROVA/q2.c
+++ QUEUE/PROVA/q2.c
@@ -9,36 +9,44 @@
 
 #define MAX_QUEUE_SIZE 100
 
-struct queue
+struct item_priority
 {
-  int front, rear;
-  int items[MAX_QUEUE_SIZE];
+  int item;
+  int priority;
 };
 
-typedef struct queue q_ed;//Definindo o tipo q_ed
+typedef struct item_priority i_p;//Item com prioridade
 
-q_ed* create_queue_ed()
+struct PQ
 {
-  q_ed *new_queue = (q_ed*)malloc(sizeof(q_ed));
+  int front, rear;
+  i_p items[MAX_QUEUE_SIZE];
+};
+
+typedef struct PQ pq_ed;//Definindo o tipo q_ed
+
+pq_ed* create_queue_ed()
+{
+  pq_ed *new_queue = (pq_ed*)malloc(sizeof(pq_ed));
   new_queue->front = 0;
   new_queue->rear = 0;
   return new_queue;//Retorna a fila
 }
 
 //Fila vazia
-int is_empty_ed(q_ed *queue)
+int is_empty_ed(pq_ed *queue)
 {
   return ((queue->front == queue->rear) ? 1 : 0);//Se está vazia retorna 1; se não, retorna 0.
 }
 
 //Função para retornar o tamanho da fila
-int size_queue(q_ed *queue)
+int size_queue(pq_ed *queue)
 {
   return queue->rear;//O tamanho é a posição do último elemento da fila.
 }
 
 //Função para enfileirar
-void enqueue_ed(q_ed *queue, int item)
+void enqueue_ed(pq_ed *queue, int item, int priority)
 {
   //Quando o tamanho atual é maior ou igual ao tamanho máximo da fila
   //Indica que a fila está cheia
@@ -49,32 +57,37 @@ void enqueue_ed(q_ed *queue, int item)
   }
   else
   {
-    queue->items[queue->rear++] = item;//Isere na posição rear e depois soma 1 para atualizar o tamanho
+    queue->items[queue->rear].item = item;//Isere na posição rear e depois soma 1 para atualizar o tamanho
+    queue->items[queue->rear].priority = priority;//Insere a prioridade
+    queue->rear++;//Soma 1 para atualizar o indice do ultimo elemento
   }
 }
 
 //Função para desenfileirar
-int dequeue_ed(q_ed *queue)
+int dequeue_ed(pq_ed *queue)
 {
-  int i, element_dequeue;
+  int i, element_dequeue, priority_element;
   if(is_empty_ed(queue))
   {
     printf("Queue empty!\n");
     exit(1);
   }
-  element_dequeue = queue->items[0];//Guardando o primeiro elemento da fila
+  element_dequeue = queue->items[0].item;//Guardando o primeiro elemento da fila
+  priority_element = queue->items[0].priority;//Guardando a prioridade do elemento
   for(i = 0; i < queue->rear; ++i)
   {
     queue->items[i] = queue->items[i+1];//Faz a fila andar - o queue->items[0] recebe o queue->items[0+1]...
+    //queue->items[i].priority = queue->items[i+1].priority;
+    //Já troca tambem a prioridade
   }
   queue->rear--;//Subtrai 1 do rear atualizando a posição do último elemento
   return element_dequeue;
 }
 
 //Função para imprimir o elemento da cabeça da fila
-int print_front_queue(q_ed *queue)
+int print_front_queue(pq_ed *queue)
 {
-  return queue->items[queue->front];
+  return queue->items[queue->front].item;
 }
 
 //Função para avaliar uma expressão
@@ -99,9 +112,9 @@ int valid_expression(char c)
 
 int main()
 {
-  int continuar = 1, i;
+  int continuar = 1, i, priority;
   char n[50];
-  q_ed *q = create_queue_ed();//Criando fila
+  pq_ed *q = create_queue_ed();//Criando fila
 
   do {
     printf("Digite um numero:\n");
@@ -121,37 +134,27 @@ int main()
     }
     int number;
     number = atoi(n);//Transforma a string cujos caracteres sao digitos em um inteiro e coloca em number
-    enqueue_ed(q,number);//Colocando numero na fila
+
+    printf("Digite a prioridade:\n");
+    scanf("%d", &priority);
+    if(priority != 0 && priority != 1)
+    {
+      printf("Prioridade invalida!\n");
+      exit(1);
+    }else
+    {
+        enqueue_ed(q,number,priority);//Colocando numero na fila
+    }
 
     printf("Deseja continuar?(0 - Nao/1 - Sim):\n");
     scanf("%d", &continuar);
   } while(continuar == 1);
 
-  q_ed *q_pair = create_queue_ed();
+  printf("Fila de prioridade:\n");
+  while(!is_empty_ed(q))
+  {
+    printf("Elemento")
+  }
 
-  for(i = 0; i < size_queue(q); ++i)
-  {
-    int element = q->items[i];
-
-    if(abs(element) % 2 == 0 && abs(element) != 0)
-    {
-      enqueue_ed(q_pair,q->items[i]);
-    }
-  }
-  printf("Fila original:\n");
-  while (!is_empty_ed(q))
-  {
-    printf("%d\n", dequeue_ed(q));
-  }
-  printf("\n");
-  printf("Fila apenas com numeros pares:\n");
-  if(is_empty_ed(q_pair)){printf("Nao ha elementos pares na fila!\n");}
-  else
-  {
-    while (!is_empty_ed(q_pair))
-    {
-      printf("%d\n", dequeue_ed(q_pair));
-    }
-  }
   return 0;
 }
