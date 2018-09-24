da6bf5b1a6f94ec790bbcf64941be7ad650d103e
NaldoHoracio <edvonaldohs@ic.ufal.br>
2018-09-24 12:35:28 -0300

    Atualização da lista encadeada.
    Verificação da pilha. Lista encadeada falta consertar a função de imprimir na forma recursiva e testar a busca e remoção, além de testes na inserção no fim e a nova função para adicionar e criar.

--- LINKED_LIST/linked_list.c
+++ LINKED_LIST/linked_list.c
@@ -2,32 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
+
+#define SIZE 5
 
 //Estrutura node
 struct Node
 {
   int item;
-  node *next_node;
+  Node *next_node;
 };
 //Definindo tipo node
 typedef struct Node node_ed;
-//Criando a lista encadeada
+//Criando a lista encadeada - modo 1
 node_ed* create_linked_list_ed()
 {
   return NULL;
+}
+
+//Criando a lista - modo 2
+node_ed* create_linked_list_ed2(node_ed *node, int item)
+{
+  node->item = item;
+  node->next_node = NULL;
+  return node;
 }
 //Lista vazia
 int is_empty_ed(node_ed *head)
 {
   return (head == NULL);
 }
-//Adicionando elemento
-node_ed* add_ed(node_ed *head, int item)
+//Adicionando elemento no começo da fila
+node_ed* add_on_head_ed(node_ed *head, int item)
 {
   node_ed *new_node = (node_ed*)malloc(sizeof(node_ed));
   new_node->item = item;
   new_node->next_node = head;
   return new_node;
+}
+//Adicionando elemento no fim da Lista
+node_ed* add_on_last_ed(node_ed *head, int item)
+{
+  node_ed *aux = (node_ed*)malloc(sizeof(node_ed));
+  aux = head;
+
+  while(aux->next_node != NULL) aux = aux->next_node;
+  node_ed *new_node = (node_ed*)malloc(sizeof(node_ed));
+  new_node->item = item;
+  new_node->next_node = NULL;
+  aux->next_node = new_node;
+  head = aux;
+  return head;
 }
 //Imprimindo a lista - forma nao recursiva
 void print_linked_list_no_recursive_ed(node_ed *head)
@@ -48,16 +73,17 @@ void print_linked_list_recursive_ed(node_ed *head)
   }
 }
 //Buscando elemento
-node_ed* search_element_ed(node_ed *head, int item)
+bool search_element_ed(node_ed *head, int item)
 {
   while(head != NULL)
   {
     if(head->item == item)
     {
-      return head;
+      return true;
     }
     head = head->next_node;
   }
+  return false;
 }
 //Removendo elemento
 node_ed* remove_node_ed(node_ed *head, int item)
@@ -82,4 +108,23 @@ node_ed* remove_node_ed(node_ed *head, int item)
   }
   free(current);
   return head;
+}
+
+int main()
+{
+  node_ed *ll_head = create_linked_list_ed();
+  node_ed *ll_last = create_linked_list_ed();
+  int n, i;
+
+  for(i = 0; i < SIZE; ++i)
+  {
+    printf("Digite um numero:\n");
+    scanf("%d", &n);
+    ll_head = add_on_head_ed(ll_head,n);
+    //add_on_last_ed(ll_last,n);
+  }
+  printf("Lista:\n");
+  print_linked_list_recursive_ed(ll_head);
+  printf("\n");
+  return 0;
 }
