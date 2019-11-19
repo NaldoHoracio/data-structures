4a78cf45be4c6184f98acdea6b256c4611c04cdd
NaldoHorácio <edvonaldohs@ic.ufal.br>
2019-11-19 05:37:38 -0300

    Atualizando lista encadeada simples.

--- LINKED_LISTS/LINKED_LIST/linked_list.c
+++ LINKED_LISTS/LINKED_LIST/linked_list.c
@@ -1,5 +1,63 @@
 #include "linked_list.h"
 
+struct node* create_linked_list()
+{
+  return NULL;
+}
+
+int is_empty(struct node *head)
+{
+  return (head == NULL);
+}
+
+struct node* add(struct node *head, int item)
+{
+  struct node *new_node = (struct node*)malloc(sizeof(struct node));
+  new_node->item = item;
+  new_node->next_node = head;
+  return new_node;
+}
+
+struct node* search(struct node *head, int item)
+{
+  while (!is_empty(head))
+  {
+    if(head->item == item)
+    {
+      return head;
+    }
+    head = head->next_node;
+  }
+  return NULL;
+  
+}
+
+struct node* remove(struct node *head, int item)
+{
+  struct node *previous = NULL;
+  struct node *current = head;
+  while(current != NULL && current->item != item)
+  {
+    previous = current;
+    current = current->next_node;
+  }
+  if(current == NULL) return head;
+  if(previous == NULL) head = current->next_node;
+  else previous->next_node = current->next_node;
+
+  free(current);
+  return head;
+}
+
+void print_linked_list(struct node *head)
+{
+  while (!is_empty(head))
+  {
+    printf("%d\n", head->item);
+    head = head->next_node;
+  } 
+}
+
 int main()
 {
   printf("Hello world!\n");
--- LINKED_LISTS/LINKED_LIST/linked_list.h
+++ LINKED_LISTS/LINKED_LIST/linked_list.h
@@ -11,16 +11,60 @@ struct node{
     struct node *next_node;
 };
 
+/** 
+ * Essa função tem o objetivo de criar a lista encadeada vazia 
+ * @param
+ *
+ * @return 
+ */
 struct node* create_linked_list();
 
+/** 
+ * Essa função tem o objetivo de adicionar um nó
+ * na cabeça da lista encadeada 
+ * @param head
+ * @param item
+ *
+ * @return 
+ */
 struct node* add(struct node *head, int item);
 
+/** 
+ * Essa função tem o objetivo de buscar um item
+ * lista encadeada 
+ * @param head
+ * @param item
+ *
+ * @return 
+ */
 struct node* search(struct node *head, int item);
 
+/** 
+ * Essa função tem o objetivo de remover um nó da lista encadeada 
+ * @param head
+ * @param item
+ *
+ * @return 
+ */
 struct node* remove(struct node *head, int item);
 
+/** 
+ * Essa função verifica se a lista está vazia
+ * @param head
+ *
+ * @return 
+ */
 int is_empty(struct node *head);
 
+
+/** 
+ * Essa função tem o objetivo de imprimir a lista encadeada
+ * a partir da cabeça da lista
+ * @param head
+ * @param item
+ *
+ * @return 
+ */
 void print_linked_list(struct node *head);
 
 
